46a3fe66d243835f6303dbad13e3d7474c9c1b1f
Pranav Putta <pranav.putta22@gmail.com>
2021-10-01 14:25:08 -0400

    moved root directory up two levels

--- Utils/FileManager.py
+++ Utils/FileManager.py
@@ -5,14 +5,12 @@ from Utils.ConfigurationLoader import Environment
 from Config.config import ROOT_DIR_
 import os
 
-current_directory = os.path.dirname(os.path.realpath(__file__))
-
 
 class FileManager:
 
     def __init__(self, env: Environment):
         """
-        Class for handling file operations (uploads and downloads, local read and write, etc.
+        Class for handling file operations (uploads and downloads, local read and write, etc.)
         """
         self.local_master_dir = env.local_file_path_root
         self.cloud_master_dir = env.cloud_file_path_root
@@ -21,7 +19,7 @@ class FileManager:
 
     def map_relative_path_to_local(self, relative_path: str):
         """
-        converts a relative file path and maps it to a local file path
+        maps relative data file path to local storage path
         @param relative_path:
         @return:
         """
@@ -29,7 +27,7 @@ class FileManager:
 
     def map_relative_path_to_cloud(self, relative_path: str):
         """
-        converts a relative file path and maps it to corresponding cloud file path
+        maps relative data file path to cloud path
         @param relative_path:
         @return: cloud path directory, file name
         """
@@ -41,7 +39,7 @@ class FileManager:
         @param relative_path: relative path to file
         @return: True if file exists in
         """
-        cloud_path, relative_name = self.map_relative_path_to_cloud(relative_path)
+        cloud_path, relative_name = os.path.split(self.map_relative_path_to_cloud(relative_path))
 
         output = subprocess.run(['rclone', 'lsf', cloud_path], capture_output=True, encoding='utf-8')
         remote_files = [x.rstrip('/') for x in output.stdout.split('\n')]
@@ -58,7 +56,8 @@ class FileManager:
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
 
-    def untar_file(self, local_path, relative_name):
+    @staticmethod
+    def untar_file(local_path, relative_name):
         """
         Given a file directory and file name, untar the file and remove the original .tar
         @param local_path: directory containing file
@@ -87,12 +86,13 @@ class FileManager:
         """
         assert relative_path is not None, "relative_path was None"
 
-        local_path, relative_name = os.path.split(self.map_relative_path_to_local(relative_path))
-        cloud_path, relative_name = os.path.split(self.map_relative_path_to_cloud(relative_path))
+        local_path_dir, name = os.path.split(self.map_relative_path_to_local(relative_path))
+        cloud_path_dir, name = os.path.split(self.map_relative_path_to_cloud(relative_path))
 
-        # pull files from corresponding directory
-        output = subprocess.run(['rclone', 'copy', f'{self.rclone_config}:{os.path.join(cloud_path, relative_name)}',
-                                 os.path.join(local_path)], capture_output=True, encoding='utf-8')
+        # pull files from cloud path to local path directory
+        output = subprocess.run(
+            ['rclone', 'copy', f'{self.rclone_config}:{os.path.join(cloud_path_dir, name)}', local_path_dir],
+            capture_output=True, encoding='utf-8')
 
         if output.stderr != '':
             print(f'There was an error downloading data {relative_path}. {output.stderr}')
@@ -100,12 +100,20 @@ class FileManager:
         # untar files if requested
         if untar:
             # check if specified document is a file or a directory
-            if os.path.isfile(os.path.join(local_path, relative_name)):
-                # if document is a file untar if requested
-                self.untar_file(local_path, relative_name)
+            if os.path.isfile(os.path.join(local_path_dir, name)):
+                # if document is a file untar it
+                self.untar_file(local_path_dir, name)
 
             else:
-                # if document is a directory, recursively traverse file directory to untar
-                path = os.path.join(local_path, relative_name)
+                # if document is a directory, traverse and untar each file
+                path = os.path.join(local_path_dir, name)
                 for f in os.listdir(path):
                     self.untar_file(path, f)
+
+    def upload_data(self):
+        # TODO: finish upload data
+        pass
+
+    def upload_data_and_merge(self):
+        # TODO: finish upload data and merge
+        pass
--- Utils/Models.py
+++ Utils/Models.py
@@ -3,5 +3,3 @@ import torch
 
 # load a model pre-trained pre-trained on COCO
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, progress=True, num_classes=3, pretrained_backbone=True)
-
-def train_ssd():
