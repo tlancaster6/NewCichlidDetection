38e4514084fefb5266f39162f86265a5ec7a0f49
Pranav Putta <pranav.putta22@gmail.com>
2021-10-01 11:02:09 -0400

    added ssd300

--- Utils/Models.py
+++ Utils/Models.py
@@ -1,6 +1,7 @@
 import torchvision.models.detection
+import torch
 
 # load a model pre-trained pre-trained on COCO
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, progress=True, num_classes=3, pretrained_backbone=True)
-model = torchvision.models.detection.ssd300_vgg16(pretrained=False)
-model.train()
+
+def train_ssd():
